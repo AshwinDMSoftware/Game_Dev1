b40b2aa8e319d8a9eb1568626a350152e4903e0d
Ashwin <ashwindmsoftware@gmail.com>
2026-09-03 16:42:33 +1000

    Ball bounces of the Bottom and Top edge. Bounces off paddles and keeps scores. Does not display score but if either score is 5 game ends

--- Test.py
+++ Test.py
@@ -7,11 +7,15 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 
+
 player_pos1 = pygame.Vector2(50, screen.get_height() / 2)
 player_pos2 = pygame.Vector2(screen.get_width()-50, screen.get_height() / 2)
 ball_pos =pygame.Vector2(screen.get_width()/2, screen.get_height() / 2)
 ball_speed_x = 3
 ball_speed_y = 3
+#Player scores
+score1 = 0
+score2 = 0
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -21,15 +25,15 @@ while running:
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
-
-    pygame.draw.rect(screen, "blue", (player_pos1.x,player_pos1.y,5,60))
+    player_1 = pygame.Rect((player_pos1.x,player_pos1.y,5,60))
+    pygame.draw.rect(screen, "blue", player_1)
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w] and player_pos1.y> 0:
         player_pos1.y -= 300 * dt
     if keys[pygame.K_s] and player_pos1.y<screen.get_height()-50:
         player_pos1.y += 300 * dt
-
-    pygame.draw.rect(screen, "red", (player_pos2.x,player_pos2.y,5,60))
+    player_2 = pygame.Rect((player_pos2.x, player_pos2.y, 5, 60))
+    pygame.draw.rect(screen, "red", player_2)
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP] and player_pos2.y> 0:
         player_pos2.y -= 300 * dt
@@ -39,17 +43,32 @@ while running:
 
     ball_pos.x += ball_speed_x
     ball_pos.y += ball_speed_y
+    #If ball hits bottom or top wall
     if ball_pos.y < 0 or ball_pos.y > screen.get_height():
         #ball_speed_x= -ball_speed_x
         ball_speed_y = -ball_speed_y
+    #If ball hits right wall
     if ball_pos.x > screen.get_width():
         ball_pos =pygame.Vector2(screen.get_width()/2, screen.get_height() / 2)
-
-    if player_pos2.y -10 > ball_pos.y > player_pos2.y +10:
-        if player_pos2.x -10 > ball_pos.x > player_pos2.x +10:
-            ball_speed_x= -ball_speed_x
-            ball_speed_y = -ball_speed_y
+        score1+= 1
+    #If ball hits left wall
+    if ball_pos.x < 0:
+        ball_pos =pygame.Vector2(screen.get_width()/2, screen.get_height() / 2)
+        score2+= 1
+    #Player collision
+    player1_hit = player_1.collidepoint(ball_pos)
+    player2_hit = player_2.collidepoint(ball_pos)
+    if player1_hit or player2_hit:
+        ball_speed_x = -ball_speed_x
+    #End State
+    if score1 > 4:
+        #Player 1 wins
+        pygame.quit()
+    if score2 > 4:
+        #Player 2 wins
+        pygame.quit()
     pygame.draw.circle(screen, "black",ball_pos,10)
+
 
     # flip() the display to put your work on screen
     pygame.display.flip()
@@ -58,6 +77,4 @@ while running:
     # dt is delta time in seconds since last frame, used for framerate-
     # independent physics.
     dt = clock.tick(60) / 1000
-#Test\
-#Test 2
 pygame.quit()
